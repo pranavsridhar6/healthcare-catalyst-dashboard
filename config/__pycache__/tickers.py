7d7804cecdfba477bd506bd305edb9a6c938f86d
pranavsridhar6 <ps1236@scarletmail.rutgers.edu>
2026-08-16 12:23:22 -0400

    Add files via upload

---
+++ config/__pycache__/tickers.py
@@ -0,0 +1,22 @@
+TICKERS = [
+    "NVO",
+    "LLY",
+    "PFE",
+    "MRK",
+    "AMGN",
+    "REGN",
+    "BMY",
+    "GILD",
+    "VRTX",
+    "BIIB",
+    "MDT",
+    "ISRG",
+    "SYK"
+]
+
+RELATED = {
+    "NVO": ["LLY"],
+    "LLY": ["NVO"],
+    "MRK": ["BMY"],
+    "BMY": ["MRK"]
+}
